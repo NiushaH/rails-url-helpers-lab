6bc631b06cbe4f6825efbac66b658bf049c64f83
Niusha H <35556570+NiushaH@users.noreply.github.com>
2021-07-19 14:17:27 -0600

    migration

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,14 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active
+    # default is false (or inactive) unless logged in / active
+    if @current_user.nil? == true
+      @current_user.logged_in? == false
+    else
+      @current_user.logged_in? == true
+    end
+  end
+
 end
