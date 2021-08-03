97d298a3b41121b544f85ec5b43b456d7f8e1d75
Niusha H <35556570+NiushaH@users.noreply.github.com>
2021-08-03 11:42:58 -0600

    lab solved

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,13 +3,13 @@ class Student < ActiveRecord::Base
     self.first_name + " " + self.last_name
   end
 
-  def active
-    # default is false (or inactive) unless logged in / active
-    if @current_user.nil? == true
-      @current_user.logged_in? == false
-    else
-      @current_user.logged_in? == true
-    end
-  end
+  # def active
+  #   # default is false (or inactive) unless logged in / active
+  #   if @current_user.nil? == true
+  #     @current_user.logged_in? == false
+  #   else
+  #     @current_user.logged_in? == true
+  #   end
+  # end
 
 end
